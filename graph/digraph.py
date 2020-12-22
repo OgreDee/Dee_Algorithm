2fdb7b001c948c5e3544092d73c759ed2ffb3dc0
chuandi.zhang <chuandi.zhang@puzzleplusgames.com>
2020-12-22 21:11:45 +0800

    拓补排序

--- graph/digraph.py
+++ graph/digraph.py
@@ -1,5 +1,6 @@
 #--encoding:utf-8--
 #有向图
+from queue import Queue
 from bag import Bag
 from graph_visualized import TreeVisualized 
 
@@ -24,7 +25,7 @@ class Digraph(object):
 			self.num_vertexCnt = vertexCnt
 			self.num_edgeCnt = 0
 			self.arr_adj = [Bag() for i in range(self.num_vertexCnt)]
-			for x in range(2,len(lines)):
+			for x in range(1,len(lines)):
 				vs = lines[x].split('-')
 				self.AddEdge(int(vs[0]), int(vs[1]))
 
@@ -55,23 +56,11 @@ class Digraph(object):
 
 
 
-#---------------------------Test--------------------------------
-str_graph = "6\n\
-0-1\n\
-0-5\n\
-0-2\n\
-1-2\n\
-2-4\n\
-2-3\n\
-3-4\n\
-3-0\n\
-3-5"
-
 
 class DigraphSearch(object):
 	graph = None
 	marked = None
-	def __init__(self, digraph, s):
+	def __init__(self, digraph):
 		super(DigraphSearch, self).__init__()
 		self.graph = digraph
 		self.marked = [False for i in range(digraph.V())]
@@ -100,8 +89,8 @@ class DirectedCycle(DigraphSearch):
 	#是有向无环图
 	beDAG = False
 
-	def __init__(self, digraph, s):
-		super(DirectedCycle, self).__init__(digraph, s)
+	def __init__(self, digraph):
+		super(DirectedCycle, self).__init__(digraph)
 		self.beDAG = True
 		for x in range(0,self.graph.V()):
 			if not self.beMarked(x):
@@ -140,13 +129,98 @@ class DirectedCycle(DigraphSearch):
 		return self.beDAG
 
 		
+class DepthFirstOrder(DigraphSearch):
+	#前序排列
+	pre = None
+	#后序排列
+	post = None
+	#逆后序排列
+	reversePost = None
+
+	def __init__(self, digraph):
+		super(DepthFirstOrder, self).__init__(digraph)
+		self.reversePost = []
+		self.pre = Queue()
+		self.post = Queue()
+		for x in range(0, digraph.V()):
+			if not self.beMarked(x):
+				self.dfs_recursion(x)
+			pass
+		self.reversePost.reverse()
+		# print("Pre: " + str(self.pre.queue))
+		# print("Post: " + str(self.post.queue))
+		# print("ReversePost: " + str(self.reversePost))
+
+	#递归式
+	def dfs_recursion(self, v):
+		self.pre.put(v)
+		self.marked[v] = True
+		for node in self.graph.arr_adj[v]:
+			if not self.beMarked(node.value):
+				self.dfs_recursion(node.value)
+		self.post.put(v)
+		self.reversePost.append(v)
+
+
+#拓扑排序必须先检查是不是DAG(有向无环图)，然后再进行逆后续排序
+class Topologicl(object):
+	#顶点的拓扑顺序
+	order = None
+	def __init__(self, digraph):
+		super(Topologicl, self).__init__()
+		cycle = DirectedCycle(digraph)
+		if cycle.isDAG():
+			orderHelper = DepthFirstOrder(digraph)
+			self.order = orderHelper.reversePost
+	
+	def Order(self):
+		return self.order
+
+	def IsDAG(self):
+		return self.order != None
+
+
+#---------------------------Test--------------------------------
+str_graph = "6\n\
+0-1\n\
+0-5\n\
+0-2\n\
+1-2\n\
+2-4\n\
+2-3\n\
+3-4\n\
+3-0\n\
+3-5"
+
+str_graph_order="13\n\
+0-6\n\
+0-1\n\
+0-5\n\
+6-9\n\
+9-10\n\
+9-12\n\
+9-11\n\
+11-12\n\
+2-0\n\
+2-3\n\
+3-5\n\
+8-7\n\
+7-6\n\
+5-4\n\
+6-4"
 
 def main():
-	digraph = Digraph(str_graph)
-	digraph.Printf("digraph_default")
-	dfs = DigraphDFS(digraph, 0)
-	cycle = DirectedCycle(digraph, 5)
-	print("DAG: "+ str(cycle.isDAG()))
+	# digraph = Digraph(str_graph)
+	# digraph.Printf("digraph_default")
+	# dfs = DigraphDFS(digraph, 0)
+	# cycle = DirectedCycle(digraph, 5)
+	# print("DAG: "+ str(cycle.isDAG()))
+
+	digraphOrder = Digraph(str_graph_order)
+	order = DepthFirstOrder(digraphOrder)
+	digraphOrder.Printf("digraph_order")
+	topologicl = Topologicl(digraphOrder)
+	print("topologicl: " + str(topologicl.Order()))
 
 	pass
 
